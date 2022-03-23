720ef350827fbd93dd66bd5fc3e713f6b1ee7faa
Ronak Patel <ronak.patel@e.email>
2022-03-23 15:02:58 +0530

    app changes

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template> 
+  <div class="welcome__label"><b>Welcome back Gael</b></div>
   <AllApps />
   <StorageLayout />
 </template>
@@ -17,16 +18,21 @@ export default {
 </script>
 
 <style>
+.welcome__label{    
+  margin-left: 2%;
+}
 #app {
-  -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  /* -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   width: 70%;
   position: absolute;
   left: 50%;
   top: 50%;
   -webkit-transform: translate(-50%, -50%);
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
+  width: 70%;
+  transform: translate(20%, 10%);
+
 }
 .welcome-label{
   color: #333333;
