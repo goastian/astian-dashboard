46e7203fa7339b078145046167b459fd9bb02e5e
Ronak P Patel <ronak.patel@e.email>
2022-03-23 13:00:21 +0000

    Update webpack.js

--- webpack.js
+++ webpack.js
@@ -10,35 +10,6 @@ module.exports = {
         filename: '[name].js?v=[hash]',
         chunkFilename: 'chunks/[name]-[hash].js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['vue-style-loader', 'css-loader'],
-            },
-            {
-                test: /\.scss$/,
-                use: ['vue-style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader',
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                loader: 'url-loader',
-                options: {
-                    name: '[name].[ext]?[hash]',
-                    limit: 8192,
-                },
-            },
-        ],
-    }, 
     resolve: {
         extensions: ['*', '.js', '.vue'],
         symlinks: false,
