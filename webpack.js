f3ce39739d7b219adb9d1af8b836ecb40f560095
Ronak P Patel <ronak.patel@e.email>
2022-03-23 12:51:38 +0000

    Update webpack.js

--- webpack.js
+++ webpack.js
@@ -1,5 +1,51 @@
-const webpackConfig = require("@nextcloud/webpack-vue-config");
-const path = require('path');
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
 
-webpackConfig.output.path = path.resolve('./dist/js');
-module.exports = webpackConfig;
+module.exports = {
+    entry: {
+        'launcher': path.join(__dirname, 'src', 'launcher.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './js'),
+        publicPath: '/js',
+        filename: '[name].js?v=[hash]',
+        chunkFilename: 'chunks/[name]-[hash].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    name: '[name].[ext]?[hash]',
+                    limit: 8192,
+                },
+            },
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.vue'],
+        symlinks: false,
+    },
+}
