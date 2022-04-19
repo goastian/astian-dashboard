e85adef941c2256d7a2c4061a84f01889fcced86
Ronak Patel <ronak.patel@e.email>
2022-04-19 12:08:45 +0530

    formatter added

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,64 @@
 <template>
-	<div id="edashboard-app">
-		<AllApps />
-		<StorageLayout />
-	</div>
+<div id="edashboard-app">
+  <AllApps />
+  <StorageLayout />
+</div>
 </template>
-
 <script>
 import AllApps from './components/AllApps.vue'
 import StorageLayout from './components/StorageLayout.vue'
-export default {
-	name: 'App',
-	components: {
-		AllApps,
-		StorageLayout,
-	},
+export default
+{
+  name: 'App',
+  components:
+  {
+    AllApps,
+    StorageLayout,
+  },
 }
+
+</script>
+<stylesheet>
+  .app-ecloud-dashboard { background-color: white; } #edashboard-app { width: 920px; margin: 10% auto; font-family: 'Roboto'; } @media only screen and (max-width: 920px) { #edashboard-app { width: 70%; } } /* Medium devices (landscape tablets, 920px and up) */ @media only screen and (min-width: 920px) { #edashboard-app { width: 920px; } }
+</stylesheet>
+
+</template>
+
+<script>
+import AllApps from './components/AllApps.vue';
+import StorageLayout from './components/StorageLayout.vue';
+export default {
+  name: 'App',
+  components: {
+    AllApps,
+    StorageLayout
+  }
+}
+
 </script>
 
 <style>
 .app-ecloud-dashboard {
-    background-color: white;
-}
-#edashboard-app {
-    width: 920px;
-    margin: 10% auto;
-    font-family: 'Roboto';
-}
-.welcome-label{
-  color: #333333;
-  font-size: 16px;
-  font-weight: 600;
-}
-@media only screen and (max-width: 920px) {
-	#edashboard-app {
-		width: 70%;
-	}
-}
-/* Medium devices (landscape tablets, 920px and up) */
-@media only screen and (min-width: 920px) {
-	#edashboard-app {
-		width: 920px;
-	}
+  background-color: white;
 }
 
+#edashboard-app {
+  width: 920px;
+  margin: 10% auto;
+  font-family: 'Roboto';
+}
+
+@media only screen and (max-width: 920px) {
+  #edashboard-app {
+    width: 70%;
+  }
+}
+
+/* Medium devices (landscape tablets, 920px and up) */
+
+@media only screen and (min-width: 920px) {
+  #edashboard-app {
+    width: 920px;
+  }
+}
 </style>
--- src/components/AllApps.vue
+++ src/components/AllApps.vue
@@ -1,37 +1,173 @@
 <template>
-	<div class="new-icons">
-		<div class="welcome__label">
-			<h2>{{ WelcomeBack }} {{ userInfo.ownerDisplayName }}</h2>
-		</div>
-		<div @click="isHidden = !isHidden">
-			<span v-if="!isHidden" class="toggle_apps show-all">{{ showAllApps }}</span>
-			<span v-if="isHidden" class="toggle_apps show-less">{{ showLessApps }}</span>
-		</div>
-		<div class="app-container">
-			<a v-for="entry in entries"
-				:key="entry.message"
-				class="item"
-				:href="entry.href">
-				<div class="color-icons" :class="entry.id" />
-				<div class="item-label"> {{ entry.name }}</div>
-			</a>
-		</div>
-		<div v-if="isHidden" class="app-container">
-			<a v-for="entry in external"
-				:key="entry.message"
-				class="item"
-				:href="entry.href">
-				<div class="color-icons" :class="entry.id" />
-				<div class="item-label"> {{ entry.name }}</div>
-			</a>
-		</div>
-	</div>
+<div class="new-icons">
+  <div class="welcome__label">
+    <h2>{{ WelcomeBack }} {{ userInfo.ownerDisplayName }}</h2>
+  </div>
+  <div @click="isHidden = !isHidden">
+    <span v-if="!isHidden"
+          class="toggle_apps show-all">{{ showAllApps }}</span>
+    <span v-if="isHidden"
+          class="toggle_apps show-less">{{ showLessApps }}</span>
+  </div>
+  <div class="app-container">
+    <a v-for="entry in entries"
+       :key="entry.message"
+       class="item"
+       :href="entry.href">
+      <div class="color-icons"
+           :class="entry.id" />
+      <div class="item-label"> {{ entry.name }}</div>
+    </a>
+  </div>
+  <div v-if="isHidden"
+       class="app-container">
+    <a v-for="entry in external"
+       :key="entry.message"
+       class="item"
+       :href="entry.href">
+      <div class="color-icons"
+           :class="entry.id" />
+      <div class="item-label"> {{ entry.name }}</div>
+    </a>
+  </div>
+</div>
+</template>
+<script>
+import axios from '@nextcloud/axios'
+import
+{
+  generateUrl
+}
+from '@nextcloud/router'
+
+export default
+{
+  name: 'AllApps',
+  data()
+  {
+    return {
+      isHidden: false,
+      entries: [],
+      external: [],
+      userInfo: [],
+      showAllApps: OC.L10N.translate('ecloud-dashboard', 'Show All Apps'),
+      showLessApps: OC.L10N.translate('ecloud-dashboard', 'Show Less Apps'),
+      WelcomeBack: OC.L10N.translate('ecloud-dashboard', 'Welcome back'),
+    }
+  },
+  mounted()
+  {
+    this.getEntries()
+    this.getDetails()
+  },
+  methods:
+  {
+    getEntries()
+    {
+      axios
+        .get(generateUrl('/apps/ecloud-dashboard/get-apps'))
+        .then(response =>
+        {
+          this.entries = response.data.apps
+          this.entries = this.entries.map(entry =>
+          {
+            entry.active = window.location.pathname.includes(
+              entry.href
+            )
+            return entry
+          })
+          this.external = this.entries.slice(6)
+          this.entries = this.entries.slice(0, 6)
+        })
+    },
+    getDetails()
+    {
+      axios
+        .get(generateUrl('/apps/files/ajax/getstoragestats.php'))
+        .then(response =>
+        {
+          this.userInfo = response.data.data
+        })
+    },
+  },
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<stylesheetsheet scoped>
+  .welcome__label{ margin-left: 0; letter-spacing: 1px; } .toggle_apps{ font-family: 'Roboto'; float: right; font-weight: 600; letter-spacing: 1px; font-size: 12px; color: #0086FF; background-repeat: no-repeat; background-position: right; padding-right: 20px; margin-right: 0px; margin-bottom: 20px; background-size: 10px; cursor: pointer; text-transform: uppercase; } .show-all{ background-image: url('../assets/down.png'); } .show-less{ background-image: url('../assets/up.png'); } .item-label{ font-size: 14px; padding-top: 10px; font-weight: 500; } .item-sublabel{ font-size: 10px; color: #949DA1; } .app-container { display: table; table-layout: fixed; width: 100%; box-sizing: border-box; } .item { vertical-align: middle; text-align: center; width: 16.6666667%; float: left; margin-top: 10px; margin-bottom: 10px; } @media only screen and (max-width: 768px) { .item { width: 33%; } } /* Medium devices (landscape tablets, 768px and up) */ @media only screen and (min-width: 768px) { .item { margin: auto; } }
+  </stylesheet>
+  </template>
+  <script>
+  import axios from '@nextcloud/axios';
+  import
+  {
+    generateUrl
+  }
+  from '@nextcloud/router';
+
+  export default
+  {
+    name: 'AllApps',
+    data()
+    {
+      return {
+        isHidden: false,
+        entries: [],
+        external: [],
+        userInfo: [],
+        showAllApps: OC.L10N.translate('ecloud-dashboard', 'Show All Apps'),
+        showLessApps: OC.L10N.translate('ecloud-dashboard', 'Show Less Apps'),
+        WelcomeBack: OC.L10N.translate('ecloud-dashboard', 'Welcome back')
+      }
+    },
+    mounted()
+    {
+      this.getEntries()
+      this.getDetails()
+    },
+    methods:
+    {
+      getEntries()
+      {
+        axios
+          .get(generateUrl('/apps/ecloud-dashboard/get-apps'))
+          .then(response =>
+          {
+            this.entries = response.data.apps
+            this.entries = this.entries.map(entry =>
+            {
+              entry.active = window.location.pathname.includes(
+                entry.href
+              )
+              return entry
+            })
+            this.external = this.entries.slice(6)
+            this.entries = this.entries.slice(0, 6)
+          })
+      },
+      getDetails()
+      {
+        axios
+          .get(generateUrl('/apps/files/ajax/getstoragestats.php'))
+          .then(response =>
+          {
+            this.userInfo = response.data.data
+          })
+      }
+    }
+  }
+
+  </script>
+  <stylesheet scoped="">
+    .welcome__label { margin-left: 0; letter-spacing: 1px; } .toggle_apps { font-family: 'Roboto'; float: right; font-weight: 600; letter-spacing: 1px; font-size: 12px; color: #0086FF; background-repeat: no-repeat; background-position: right; padding-right: 20px; margin-right: 0px; margin-bottom: 20px; background-size: 10px; cursor: pointer; text-transform: uppercase; } .show-all { background-image: url('../assets/down.png'); } .show-less { background-image: url('../assets/up.png'); } .item-label { font-size: 14px; padding-top: 10px; font-weight: 500; } .item-sublabel { font-size: 10px; color: #949DA1; } .app-container { display: table; table-layout: fixed; width: 100%; box-sizing: border-box; } .item { vertical-align: middle; text-align: center; width: 16.6666667%; float: left; margin-top: 10px; margin-bottom: 10px; } @media only screen and (max-width: 768px) { .item { width: 33%; } } /* Medium devices (landscape tablets, 768px and up) */ @media only screen and (min-width: 768px) { .item { margin: auto; } }
+  </stylesheet>
+
 </template>
 
 <script>
-
-import axios from '@nextcloud/axios'
-import { generateUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios';
+import { generateUrl } from '@nextcloud/router';
 
 export default {
   name: 'AllApps',
@@ -43,10 +179,10 @@ export default {
       userInfo: [],
       showAllApps: OC.L10N.translate('ecloud-dashboard', 'Show All Apps'),
       showLessApps: OC.L10N.translate('ecloud-dashboard', 'Show Less Apps'),
-      WelcomeBack: OC.L10N.translate('ecloud-dashboard', 'Welcome back'),
+      WelcomeBack: OC.L10N.translate('ecloud-dashboard', 'Welcome back')
     }
   },
-mounted() {
+  mounted() {
     this.getEntries()
     this.getDetails()
   },
@@ -72,71 +208,8 @@ mounted() {
         .then(response => {
           this.userInfo = response.data.data
         })
-    },
-  },
+    }
+  }
 }
-</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.welcome__label{
-  margin-left: 0;
-  letter-spacing: 1px;
-}
-.toggle_apps{
-  font-family: 'Roboto';
-  float: right;
-  font-weight: 600;
-  letter-spacing: 1px;
-  font-size: 12px;
-  color: #0086FF;
-  background-repeat: no-repeat;
-  background-position: right;
-  padding-right: 20px;
-  margin-right: 0px;
-  margin-bottom: 20px;
-  background-size: 10px;
-  cursor: pointer;
-  text-transform: uppercase;
-}
-.show-all{
-  background-image: url('../assets/down.png');
-}
-.show-less{
-  background-image: url('../assets/up.png');
-}
-.item-label{
-  font-size: 14px;
-  padding-top: 10px;
-  font-weight: 500;
-}
-.item-sublabel{
-  font-size: 10px;
-  color: #949DA1;
-}
-.app-container {
-  display: table;
-  table-layout: fixed;
-  width: 100%;
-  box-sizing: border-box;
-}
-.item {
-  vertical-align: middle;
-  text-align: center;
-  width: 16.6666667%;
-  float: left;
-  margin-top: 10px;
-  margin-bottom: 10px;
-}
-@media only screen and (max-width: 768px) {
-	.item {
-		width: 33%;
-	}
-}
-/* Medium devices (landscape tablets, 768px and up) */
-@media only screen and (min-width: 768px) {
-	.item {
-		margin: auto;
-	}
-}
-</style>
+</script>
