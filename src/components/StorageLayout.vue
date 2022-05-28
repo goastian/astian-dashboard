29da5b5a9fc70d242629fa876bd2154aecfda5e7
Arnau Vàzquez <arnauvp@e.email>
2022-05-28 13:32:24 +0000

    Check also empty links

--- src/components/StorageLayout.vue
+++ src/components/StorageLayout.vue
@@ -16,7 +16,7 @@
 					<div class="usage-info">
 						{{ usageinfo }}
 					</div>
-					<div v-if="storageInfo.quota > 0" class="upgrade-storage__div">
+					<div v-if="storageLink && storageInfo.quota > 0" class="upgrade-storage__div">
 						<a id="upgrade-btn" :href="storageLink">
 							{{ upgradeStorage }}
 						</a>
@@ -112,7 +112,7 @@ export default {
 			axios
 				.get(generateUrl('/apps/ecloud-dashboard/apps/get-redirections'))
 				.then((response) => {
-					this.storageLink = response.data.storageLink
+					this.storageLink = response.data.storageLink || ''
 					this.redirectURL = response.data.redirectURL
 				})
 		},
