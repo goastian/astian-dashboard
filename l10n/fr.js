3888c970aed3aeea28f91607d60354ccc81c095b
Aude M <aude.madkaud@murena.com>
2023-03-23 16:10:46 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: ecloud/Murena Dashboard
    Translate-URL: https://i18n.e.foundation/projects/ecloud/murena-dashboard/fr/

--- l10n/fr.js
+++ l10n/fr.js
@@ -2,6 +2,12 @@ OC.L10N.register(
     "ecloud-dashboard",
     {
     "Error when trying to connect to ONLYOFFICE" : "Erreur lors d'une tentative de connexion à ONLYOFFICE",
+    "Document" : "Document",
+    "Spreadsheet" : "Feuille de calcul",
+    "Presentation" : "Présentation",
+    "Murena dashboard" : "Tableau de bord Murena",
+    "Murena Dashboard" : "Tableau de bord Murena",
+    "Murena Cloud Dashboard" : "Tableau de bord du Cloud Murena",
     "Welcome back" : "Content de vous revoir",
     "Show All Apps" : "Afficher toutes les applications",
     "Show Less Apps" : "Afficher moins d'applications",
