2dbd4269fcd50958024679b2abc38582e416fcec
Avinash Gusain <avinashg@e.email>
2023-04-27 23:25:39 +0530

    scroll fix

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@ export default {
 </script>
 
 <style scoped>
-#dashboard-app >>> .app-murena-dashboard {
+::v-deep .app-murena-dashboard {
 	z-index: 1000;
 	background-color: var(--color-main-background) !important;
 	position: relative;
