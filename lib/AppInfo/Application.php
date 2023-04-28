6e657bfd596d09422b2058e3282c148d709bba9c
Avinash Gusain <avinashg@e.email>
2023-04-28 16:03:54 +0530

    css added

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -3,10 +3,22 @@
 namespace OCA\MurenaDashboard\AppInfo;
 
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
+use OCA\MurenaLauncher\Listeners\BeforeTemplateRenderedListener;
 
 class Application extends App {
 	public function __construct(array $urlParams = array()) {
 		$appName = "murena-dashboard";
 		parent::__construct($appName, $urlParams);
 	}
+	public function register(IRegistrationContext $context): void {
+		$context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
+	}
+
+	public function boot(IBootContext $context): void {
+	}
+
 }
