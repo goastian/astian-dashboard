b968da6ebc31e27eae64eec8435e346398d6d47b
Avinash Gusain <avinashg@e.email>
2023-04-28 16:31:34 +0530

    listner added

--- lib/Listeners/BeforeTemplateRenderedListener.php
+++ lib/Listeners/BeforeTemplateRenderedListener.php
@@ -1,34 +1,43 @@
 <?php
 
+
 namespace OCA\MurenaDashboard\Listeners;
 
 use OCP\EventDispatcher\Event;
 use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
-use OCP\EventDispatcher\IEventListener;
+use \OCP\EventDispatcher\IEventListener;
 use OCP\Util;
 use OCP\IUserSession;
+use OCA\MurenaLauncher\Service\AppsService;
+use OCP\INavigationManager;
+use OCP\App\IAppManager;
 
-class BeforeTemplateRenderedListener implements IEventListener
-{
-    private Util $util;
-    private IUserSession $userSession;
-    private string $appName;
+class BeforeTemplateRenderedListener implements IEventListener {
+	private Util $util;
+	private IUserSession $userSession;
+	private INavigationManager $navigationManager;
+	private AppsService $appsService;
+	private IAppManager $appManager;
+	private string $appName;
 
-    public function __construct($appName, Util $util, IUserSession $userSession)
-    {
-        $this->appName = $appName;
-        $this->util = $util;
-        $this->userSession = $userSession;
-    }
+	private const ONLYOFFICE_APP_ID = 'onlyoffice';
 
-    public function handle(Event $event): void
-    {
-        if (!($event instanceof BeforeTemplateRenderedEvent)) {
-            return;
-        }
+	public function __construct($appName, Util $util, IUserSession $userSession, INavigationManager $navigationManager, AppsService $appsService) {
+		$this->appName = $appName;
+		$this->util = $util;
+		$this->userSession = $userSession;
+		$this->navigationManager = $navigationManager;
+		$this->appsService = $appsService;
+		$this->appManager = $appManager;
+	}
 
-        if ($this->userSession->isLoggedIn()) {
-            $this->util->addStyle($this->appName, 'murena-dashboard');
-        }
-    }
+	public function handle(Event $event): void {
+		if (!($event instanceof BeforeTemplateRenderedEvent)) {
+			return;
+		}
+		if ($this->userSession->isLoggedIn()) {
+			$this->util->addStyle($this->appName, 'murena-dashboard');
+		}
+	}
+
 }
