65fb3ee10e2ba4d893fa538b1e0a9a2eb0bfd284
Avinash Gusain <avinashg@e.email>
2023-04-28 16:37:30 +0530

    listner added

--- lib/Listeners/BeforeTemplateRenderedListener.php
+++ lib/Listeners/BeforeTemplateRenderedListener.php
@@ -8,27 +8,16 @@ use OCP\AppFramework\Http\Events\BeforeTemplateRenderedEvent;
 use \OCP\EventDispatcher\IEventListener;
 use OCP\Util;
 use OCP\IUserSession;
-use OCA\MurenaLauncher\Service\AppsService;
-use OCP\INavigationManager;
-use OCP\App\IAppManager;
 
 class BeforeTemplateRenderedListener implements IEventListener {
 	private Util $util;
 	private IUserSession $userSession;
-	private INavigationManager $navigationManager;
-	private AppsService $appsService;
-	private IAppManager $appManager;
 	private string $appName;
 
-	private const ONLYOFFICE_APP_ID = 'onlyoffice';
-
-	public function __construct($appName, Util $util, IUserSession $userSession, INavigationManager $navigationManager, AppsService $appsService) {
+	public function __construct($appName, Util $util, IUserSession $userSession) {
 		$this->appName = $appName;
 		$this->util = $util;
 		$this->userSession = $userSession;
-		$this->navigationManager = $navigationManager;
-		$this->appsService = $appsService;
-		$this->appManager = $appManager;
 	}
 
 	public function handle(Event $event): void {
