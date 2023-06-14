aca41eb2a2f62aa3cfc812fbe209e9c0b665b388
Ronak <ronak@Ronaks-MacBook-Pro.local>
2023-06-13 18:18:04 -0700

    added variable

--- src/components/AllApps.vue
+++ src/components/AllApps.vue
@@ -33,7 +33,7 @@
 				<span v-if="showAllApps" class="toggle_apps show-less">{{ t(appName, 'Show Less Apps') }}</span>
 			</div>
 			<div class="app-container">
-				<a v-for="entry in entries.slice(0,12)"
+				<a v-for="entry in entries.slice(0,defaultApps)"
 					:key="entry.message"
 					:class="{ 'beta-app': entry.is_beta, 'item': true }"
 					:href="entry.href"
@@ -50,7 +50,7 @@
 				</a>
 			</div>
 			<div v-if="showAllApps" class="app-container">
-				<a v-for="entry in entries.slice(12)"
+				<a v-for="entry in entries.slice(defaultApps)"
 					:key="entry.message"
 					:class="{ 'beta-app': entry.is_beta, 'item': true }"
 					:href="entry.href"
@@ -76,6 +76,7 @@ export default {
 	name: 'AllApps',
 	data() {
 		return {
+      defaultApps: 12,
 			showAllApps: false,
 			entries: loadState('murena-dashboard', 'entries'),
 			displayName: loadState('murena-dashboard', 'displayName'),
