19f14165fdebe46314ecb2aedc2f381fcd84c2ba
Ronak Patel <ronak.patel@e.email>
2023-07-20 07:57:27 +0000

    Position issue in dashboard

--- lib/Service/AppsService.php
+++ lib/Service/AppsService.php
@@ -23,7 +23,7 @@ class AppsService {
 	private IURLGenerator $urlGenerator;
 	private IRootFolder $rootFolder;
 
-	private const DEFAULT_ORDER = array("/apps/snappymail/", "/apps/calendar/", "/apps/files/" , "/apps/photos/", "/apps/memories/", "/apps/contacts/", "/apps/onlyoffice/ajax/new?id=onlyoffice_docx", "/apps/onlyoffice/ajax/new?id=onlyoffice_xlsx", "/apps/onlyoffice/ajax/new?id=onlyoffice_pptx", "/apps/notes/", "/apps/tasks/", "https://spot.murena.io" , "https://murena.com" );
+	private const DEFAULT_ORDER = array("/apps/snappymail/", "/apps/calendar/", "/apps/files/" , "/apps/photos/", "/apps/memories/", "/apps/contacts/", "/apps/onlyoffice/new?id=onlyoffice_docx", "/apps/onlyoffice/new?id=onlyoffice_xlsx", "/apps/onlyoffice/new?id=onlyoffice_pptx", "/apps/notes/", "/apps/tasks/", "https://spot.murena.io" , "https://murena.com" );
 	public function __construct(
 		$appName,
 		IConfig $config,
