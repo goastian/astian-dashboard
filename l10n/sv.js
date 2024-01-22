28ee841bd7fdbdd8e5a488caec63ba8dd16f81e8
Kristian Nordin <kristian.po@proton.me>
2024-01-22 21:46:55 +0000

    Translated using Weblate (Swedish)
    Currently translated at 76.9% (10 of 13 strings)
    
    Translation: ecloud/Murena Dashboard
    Translate-URL: https://i18n.e.foundation/projects/ecloud/murena-dashboard/sv/

---
+++ l10n/sv.js
@@ -0,0 +1,15 @@
+OC.L10N.register(
+    "murena-dashboard",
+    {
+    "Error when trying to connect to ONLYOFFICE" : "Fel vid försök att ansluta till ONLYOFFICE",
+    "Document" : "Dokument",
+    "Spreadsheet" : "Kalkylblad",
+    "Presentation" : "Presentation",
+    "Welcome back" : "Välkommen tillbaka",
+    "Show All Apps" : "Visa alla appar",
+    "Show Less Apps" : "Visa färre appar",
+    "Storage" : "Lagring",
+    "Upgrade Storage" : "Uppgradera lagring",
+    "Invite Your Friends" : "Bjud in dina vänner"
+},
+"nplurals=2; plural=n != 1;");
