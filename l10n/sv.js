ca0da77279a7c2c5783cb280f5206f7bcfad5476
Kristian Nordin <kristian.po@proton.me>
2024-01-24 21:10:39 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: ecloud/Murena Dashboard
    Translate-URL: https://i18n.e.foundation/projects/ecloud/murena-dashboard/sv/

--- l10n/sv.js
+++ l10n/sv.js
@@ -5,11 +5,14 @@ OC.L10N.register(
     "Document" : "Dokument",
     "Spreadsheet" : "Kalkylblad",
     "Presentation" : "Presentation",
+    "Murena Dashboard" : "Murena Infopanel",
     "Welcome back" : "Välkommen tillbaka",
     "Show All Apps" : "Visa alla appar",
     "Show Less Apps" : "Visa färre appar",
     "Storage" : "Lagring",
     "Upgrade Storage" : "Uppgradera lagring",
+    "getCredits" : "getCredits",
+    "openAnAccount" : "openAnAccount",
     "Invite Your Friends" : "Bjud in dina vänner"
 },
 "nplurals=2; plural=n != 1;");
