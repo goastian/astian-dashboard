611872ffeac469eb18a6d6276b8acb6039bc11e9
kivk shozk <kivk@users.noreply.i18n.e.foundation>
2024-03-25 14:56:18 +0000

    Translated using Weblate (Japanese)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: ecloud/Murena Dashboard
    Translate-URL: https://i18n.e.foundation/projects/ecloud/murena-dashboard/ja/

---
+++ l10n/ja.js
@@ -0,0 +1,18 @@
+OC.L10N.register(
+    "murena-dashboard",
+    {
+    "Error when trying to connect to ONLYOFFICE" : "ONLYOFFICEに接続する際にエラーが発生しました",
+    "Document" : "ドキュメント",
+    "Spreadsheet" : "スプレッドシート",
+    "Presentation" : "プレゼンテーション",
+    "Murena Dashboard" : "Murenaダッシュボード",
+    "Welcome back" : "おかえりなさい",
+    "Show All Apps" : "全てのアプリを表示",
+    "Show Less Apps" : "一部のアプリのみを表示",
+    "Storage" : "ストレージ",
+    "Upgrade Storage" : "ストレージをアップグレード",
+    "getCredits" : "友達を招待すると、最高40ユーロのクラウドストレージ用クレジットをプレゼントします！",
+    "openAnAccount" : "あなたの友達がアカウントを登録すると、1人あたり2ユーロのMurenaクラウドのストレージ用クレジットをあなたにプレゼントします。",
+    "Invite Your Friends" : "友達を招待"
+},
+"nplurals=1; plural=0;");
